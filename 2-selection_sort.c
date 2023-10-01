917ab967a6fc13b00d8911c309a813bd1e504c2a
milkawinoh <milkawinoh>
2023-10-01 05:59:30 +0300

    g

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,36 +1,34 @@
- #include "sort.h"                                                               
-  2                                                                                 
-  3 /**                                                                             
-  4  * selection_sort - Sorts an array of integers in ascending order using         
-  5  * the Selection Sort algorithm.                                                
-  6  * @array: The array to be sorted.                                              
-  7  * @size: The size of the array.                                                
-  8  */                                                                             
-  9 void selection_sort(int *array, size_t size)                                    
- 10 {                                                                               
- 11         unsigned int min, i, j, temp;                                           
- 12                                                                                 
- 13         for (i = 0; i < size - 1; i++)                                          
- 14         {                                                                       
- 15                 min = i;                                                        
- 16                 for (j = i + 1; j < size; j++)                                  
- 17                 {                                                               
- 18                         if (array[j] < array[min])                              
- 19                         {                                                       
- 20                                 min = j;                                        
- 21                         }                                                       
- 22                 }                                                               
- 23                                                                                 
- 24                 if (min != i)                                                   
- 25                 {                                                               
- 26                         temp = array[i];                                        
- 27                         array[i] = array[min];                                  
- 28                         array[min] = temp;                                      
- 29                                                                                 
- 30                         print_array(array, size);                               
- 31                 }                                                               
- 32         }                                                                       
- 33 }                                                                               
-~                                                                                         
-~                                                                                         
-~                                               
+ #include "sort.h"                                                                                                                                             
+   /**                                                                             
+    * selection_sort - Sorts an array of integers in ascending order using         
+    * the Selection Sort algorithm.                                                
+    * @array: The array to be sorted.                                              
+    * @size: The size of the array.                                                
+    */                                                                             
+   void selection_sort(int *array, size_t size)                                    
+  {                                                                               
+          unsigned int min, i, j, temp;                                           
+                                                                                  
+          for (i = 0; i < size - 1; i++)                                          
+          {                                                                       
+                  min = i;                                                        
+                  for (j = i + 1; j < size; j++)                                  
+                  {                                                               
+                          if (array[j] < array[min])                              
+                          {                                                       
+                                  min = j;                                        
+                          }                                                       
+                  }                                                               
+                                                                                  
+                  if (min != i)                                                   
+                  {                                                               
+                          temp = array[i];                                        
+                          array[i] = array[min];                                  
+                          array[min] = temp;                                      
+                                                                                  
+                          print_array(array, size);                               
+                  }                                                               
+          }                                                                       
+  }                                                                               
+                                                                                       
+                                                                                                                                       
